c5872c4487ada7482d75fd6582c449439374926b
vestamiko <vestamikolajunaite@gmail.com>
2026-02-25 11:10:21 +0200

    first commit

--- routes/dishRoutes.js
+++ routes/dishRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { createDish } = require("../controllers/dishControllers.js");
+const { createDish, 
+    getAllDishes,
+    getOneDish,
+    updateDish,
+    deleteDish,
+ } = require("../controllers/dishControllers.js");
 const { protectAdmin } = require("../middlewares/adminAuthMiddleware.js");
 
 router.route("/").post(protectAdmin, createDish);
+router.route("/all").post(getAllDishes);
+router.route("/:id")
+.get(getOneDish)
+.put(protectAdmin, updateDish)
+.delete(protectAdmin, deleteDish);
 
 module.exports = router;
